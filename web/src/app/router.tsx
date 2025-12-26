d55ee75e63f390d49067022f29b3bdb0b913bcac
artem4ik <artem_svinoboev@mail.ru>
2025-12-26 22:09:59 +0900

    update

---
+++ web/src/app/router.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
+import Sessions from "../pages/Sessions";
+import SessionDetail from "../pages/SessionDetail";
+
+export const router = createBrowserRouter([
+  { path: "/", element: <Dashboard /> },
+  { path: "/sessions", element: <Sessions /> },
+  { path: "/sessions/:id", element: <SessionDetail /> },
+]);
